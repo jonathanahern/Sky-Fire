78017f3c2b306bb403f992b5ca21d1fe323f56f1
Chris Chung <christopher0chung@gmail.com>
2016-10-25 12:56:46 -0400

    Camera functionality with Q and E

---
+++ Sky Fire/Assets/CameraPivotBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPivotBehavior : MonoBehaviour {
+
+    public float turnRate;
+    public float turnPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            turnPos -= 45;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            turnPos += 45;
+        }
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, turnPos, 0), turnRate);
+
+    }
+}
