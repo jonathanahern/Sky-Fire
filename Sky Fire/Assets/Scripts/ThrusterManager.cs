b560a75126977d90b936b96cdb74d3bc09b22bae
jonathanahern <jda409@nyu.edu>
2016-10-28 15:42:39 -0400

    Movement buttons working

---
+++ Sky Fire/Assets/Scripts/ThrusterManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterManager : MonoBehaviour {
+
+	public ThrusterScript AftPortTop;
+	public ThrusterScript AftStbdTop;
+	public ThrusterScript AftPortBot;
+	public ThrusterScript AftStbdBot;
+	public ThrusterScript FwdStbdBot;
+	public ThrusterScript FwdPortBot;
+	public ThrusterScript FwdStbdTop;
+	public ThrusterScript FwdPortTop;
+
+	public void AftTop () {
+		if (AftStbdTop.onOff != AftPortTop.onOff) {
+				AftPortTop.TurnOnFunction ();
+				AftStbdTop.TurnOnFunction ();
+		} else {
+			AftPortTop.OnOffFunction ();
+			AftStbdTop.OnOffFunction ();
+		}
+	}
+	public void AftBot () {
+		if (AftPortBot.onOff != AftStbdBot.onOff) {
+			AftPortBot.TurnOnFunction ();
+			AftStbdBot.TurnOnFunction ();
+		} else {
+			AftPortBot.OnOffFunction ();
+			AftStbdBot.OnOffFunction ();
+		}
+	}
+	public void AftPort () {
+		if (AftPortTop.onOff != AftPortBot.onOff) {
+			AftPortTop.TurnOnFunction ();
+			AftPortBot.TurnOnFunction ();
+		} else {
+			AftPortTop.OnOffFunction ();
+			AftPortBot.OnOffFunction ();
+		}
+	}
+	public void AftStrb () {
+		if (AftStbdTop.onOff != AftStbdBot.onOff) {
+			AftStbdTop.TurnOnFunction ();
+			AftStbdBot.TurnOnFunction ();
+		} else {
+			AftStbdTop.OnOffFunction ();
+			AftStbdBot.OnOffFunction ();
+		}
+	}
+	public void FwdTop () {
+		if (FwdStbdTop.onOff != FwdPortTop.onOff) {
+			FwdStbdTop.TurnOnFunction ();
+			FwdPortTop.TurnOnFunction ();
+		} else {
+			FwdStbdTop.OnOffFunction ();
+			FwdPortTop.OnOffFunction ();
+		}
+	}
+	public void FwdBot () {
+		if (FwdStbdBot.onOff != FwdPortBot.onOff) {
+			FwdStbdBot.TurnOnFunction ();
+			FwdPortBot.TurnOnFunction ();
+		} else {
+			FwdStbdBot.OnOffFunction ();
+			FwdPortBot.OnOffFunction ();
+		}
+	}
+	public void FwdPort () {
+		if (FwdPortTop.onOff != FwdPortBot.onOff) {
+			FwdPortTop.TurnOnFunction ();
+			FwdPortBot.TurnOnFunction ();
+		} else {
+			FwdPortTop.OnOffFunction ();
+			FwdPortBot.OnOffFunction ();
+		}
+	}
+	public void FwdStrb () {
+		if (FwdStbdTop.onOff != FwdStbdBot.onOff) {
+			FwdStbdTop.TurnOnFunction ();
+			FwdStbdBot.TurnOnFunction ();
+		} else {
+			FwdStbdTop.OnOffFunction ();
+			FwdStbdBot.OnOffFunction ();
+		}
+	}
+
+}
--- Sky Fire/Assets/Scripts/ThrusterScript.cs
+++ Sky Fire/Assets/Scripts/ThrusterScript.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ThrusterScript : MonoBehaviour {
 
     public KeyCode inputKey;
+	public Image myButton;
+	public Image doubleLeftButton;
+	public Image doubleRightButton;
+	public ThrusterScript thrusterLeft;
+	public ThrusterScript thrusterRight;
 
     public bool onOff;
     public float forceScalar;
@@ -13,11 +19,18 @@ public class ThrusterScript : MonoBehaviour {
     private SpriteRenderer mySR;
     private int counter;
 
+	public Color offColor;
+	public Color onColor;
+
 	// Use this for initialization
 	void Start () {
         myPS = GetComponentInChildren<ParticleSystem>();
         myRB = transform.root.gameObject.GetComponent<Rigidbody>();
         mySR = GetComponentInChildren<SpriteRenderer>();
+
+		offColor = new Color (1, 1, 1, 0.42f);
+		onColor = new Color (0, 1, 1, 0.5f);
+
     }
 	
 	// Update is called once per frame
@@ -47,4 +60,63 @@ public class ThrusterScript : MonoBehaviour {
         }
 
     }
+
+	public void OnOffFunction() {
+		if (!onOff) {
+			onOff = true;
+			myButton.color = onColor;
+			NeighborCheck ();
+
+		} else {
+			onOff = false;
+			myButton.color = offColor;
+			NeighborCheck ();
+
+		}
+	}
+
+	public void TurnOnFunction(){
+
+		if (!onOff) {
+			onOff = true;
+			myButton.color = onColor;
+			NeighborCheck ();
+		}
+	}
+
+
+	void NeighborCheck (){
+	
+		if (onOff == thrusterLeft.onOff && onOff == true && doubleLeftButton.color != onColor) {
+			
+			doubleLeftButton.color = onColor;
+		}
+
+		if (onOff == thrusterRight.onOff && onOff == true && doubleRightButton.color != onColor) {
+
+			doubleRightButton.color = onColor;
+		}
+
+		if (onOff == thrusterLeft.onOff && onOff == false && doubleLeftButton.color != offColor) {
+
+			doubleLeftButton.color = offColor;
+		}
+
+		if (onOff == thrusterRight.onOff && onOff == false && doubleRightButton.color != offColor) {
+			
+			doubleRightButton.color = offColor;
+		}
+
+		if (onOff != thrusterRight.onOff && doubleRightButton.color != offColor) {
+		
+			doubleRightButton.color = offColor;		
+
+		}
+
+		if (onOff != thrusterLeft.onOff && doubleLeftButton.color != offColor) {
+
+			doubleLeftButton.color = offColor;		
+
+		}
+	}
 }
