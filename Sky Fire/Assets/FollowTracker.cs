99b8d9b99b7f9cfe6d56bce3f765af29dd91682f
Chris Chung <christopher0chung@gmail.com>
2016-11-01 15:10:18 -0400

    Better prediction.

---
+++ Sky Fire/Assets/FollowTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTracker : MonoBehaviour {
+
+    public int elementNumber;
+    private Vector3[] theList;
+
+	// Use this for initialization
+	void Start () {
+
+        theList = transform.root.gameObject.GetComponent<RigidBodyBehavior>().myPredictions;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = theList[elementNumber];
+	
+	}
+}
--- Sky Fire/Assets/RigidBodyBehavior.cs
+++ Sky Fire/Assets/RigidBodyBehavior.cs
@@ -4,57 +4,79 @@ using System.Collections;
 public class RigidBodyBehavior : MonoBehaviour {
 
     Rigidbody myRB;
-    public Vector3 myAngVel;
-    public Vector3 myVel;
+
+    public static int secondsOfPrediction;
+
+    private Vector3 myAngVel;
+    public Vector3 myAngVelPublic;
+    private Vector3 myAngVelOld;
+    private Vector3 myTorqueInst;
+    private Vector3 myTorqueSmoothPass1;
+    public Vector3 myTorquePublic;
+
+   
+
+
+    private Vector3 myVel;
     public Vector3 myVelPublic;
     private Vector3 myVelOld;
     private Vector3 myAccelInst;
     private Vector3 myAccelSmoothPass1;
-    public Vector3 myAccel;
+    public Vector3 myAccelPublic;
 
     public float smoothRate;
 
-
-    public GameObject pt0;
-    public GameObject pt1;
-    public GameObject pt2;
-    public GameObject pt3;
+    public Vector3[] myPredictions = new Vector3[secondsOfPrediction + 1];
+    //public GameObject pt0;
+    //public GameObject pt1;
+    //public GameObject pt2;
+    //public GameObject pt3;
 
 	// Use this for initialization
 	void Start () {
 
         myRB = GetComponent<Rigidbody>();
+        for (int i = 0; i < myPredictions.Length; i++)
+        {
+            myPredictions[i] = Vector3.zero;
+        }
 
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+    void Update()
+    {
+        myTorqueSmoothPass1 = Vector3.Lerp(myTorqueSmoothPass1, myTorqueInst, smoothRate);
+        myAccelSmoothPass1 = Vector3.Lerp(myAccelSmoothPass1, myAccelInst, smoothRate);
+    }
 
-        myAngVel = new Vector3 (Mathf.Round(myRB.angularVelocity.x * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myRB.angularVelocity.y * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myRB.angularVelocity.z * Mathf.Rad2Deg * 100) / 100);
+    void FixedUpdate () {
+
+        myAngVelOld = myAngVel;
+        myAngVel = myRB.angularVelocity;
+
+        myTorqueInst = (myAngVel - myAngVelOld) / Time.fixedDeltaTime;
+
+        myTorquePublic = new Vector3(Mathf.Round(myTorqueSmoothPass1.x * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myTorqueSmoothPass1.y * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myTorqueSmoothPass1.z * Mathf.Rad2Deg * 100) / 100);
+        myAngVelPublic = new Vector3(Mathf.Round(myAngVel.x * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myAngVel.y * Mathf.Rad2Deg * 100) / 100, Mathf.Round(myAngVel.z * Mathf.Rad2Deg * 100) / 100);
+
+
         myVelOld = myVel;
         myVel = myRB.velocity;
 
         myAccelInst = (myVel - myVelOld) / Time.fixedDeltaTime;
 
-        myAccelSmoothPass1 = Vector3.Lerp(myAccelSmoothPass1, myAccelInst, .08f);
-
-        myAccel = new Vector3(Mathf.Round(myAccelSmoothPass1.x * 100) / 100, Mathf.Round(myAccelSmoothPass1.y * 100) / 100, Mathf.Round(myAccelSmoothPass1.z * 100) / 100);
+        myAccelPublic = new Vector3(Mathf.Round(myAccelSmoothPass1.x * 100) / 100, Mathf.Round(myAccelSmoothPass1.y * 100) / 100, Mathf.Round(myAccelSmoothPass1.z * 100) / 100);
         myVelPublic = new Vector3(Mathf.Round(myVel.x * 100) / 100, Mathf.Round(myVel.y * 100) / 100, Mathf.Round(myVel.z * 100) / 100);
 
 
-        pt0.transform.position = transform.position;
-        pt0.transform.localRotation = Quaternion.Euler(myAngVel);
+        for (int i = 0; i < myPredictions.Length; i++)
+        {
+            Quaternion myAngVelApplied = Quaternion.Euler(myAngVel * i);
+            Quaternion myAngAccelApplied = Quaternion.Euler(myTorqueSmoothPass1 * i * i * 0.5f);
 
-        pt1.transform.position = Vector3.Lerp(pt1.transform.position, pt0.transform.position + myVel + (.5f * myAccel), smoothRate);
-        pt1.transform.localRotation = Quaternion.Euler(myAngVel);
-
-        pt2.transform.position = Vector3.Lerp(pt2.transform.position, pt1.transform.position + myVel + (.5f * myAccel * 4), smoothRate);
-        pt2.transform.localRotation = Quaternion.Euler(myAngVel);
-
-        pt3.transform.position = Vector3.Lerp(pt3.transform.position, pt2.transform.position + myVel + (.5f * myAccel * 9), smoothRate);
-        pt3.transform.localRotation = Quaternion.Euler(myAngVel);
-
-
+            myPredictions[i] = transform.position + (myAngVelApplied * (myVel * i)) + (myAngAccelApplied * (myAccelSmoothPass1 * i * i * 0.5f));
+        }
 
     }
 }
