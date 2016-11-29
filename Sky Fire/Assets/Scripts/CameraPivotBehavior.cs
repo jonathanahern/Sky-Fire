c2753a39da104b3d72341464cc6ab4e67fe319d0
Chris Chung <christopher0chung@gmail.com>
2016-11-28 22:59:34 -0500

    Right-click to move cam.  Double right-click to reset cam.

--- Sky Fire/Assets/Scripts/CameraPivotBehavior.cs
+++ Sky Fire/Assets/Scripts/CameraPivotBehavior.cs
@@ -4,26 +4,71 @@ using System.Collections;
 public class CameraPivotBehavior : MonoBehaviour {
 
     public float turnRate;
-    public float turnPos;
+    //public float turnPos;
 
-	// Use this for initialization
-	void Start () {
+    private float yawAng;
+    private float pitchAng;
+
+    public float xDeltaFactor;
+    public float yDeltaFactor;
+
+    private float oldMouseX;
+    private float oldMouseY;
+
+    private float resetFlagTimer;
+    public float resetTimeInterval;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    turnPos -= 45;
+        //}
+        //else if (Input.GetKeyDown(KeyCode.X))
+        //{
+        //    turnPos += 45;
+        //}
+        resetFlagTimer += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(1))
         {
-            turnPos -= 45;
+            if (resetFlagTimer <= resetTimeInterval)
+            {
+                yawAng = 0;
+                pitchAng = 0;
+            }
+            Vector2 mousePos = Input.mousePosition;
+            oldMouseX = mousePos.x;
+            oldMouseY = mousePos.y;
+            resetFlagTimer = 0;
         }
-        else if (Input.GetKeyDown(KeyCode.X))
+        else if (Input.GetMouseButton(1))
         {
-            turnPos += 45;
+            Vector2 mousePos = Input.mousePosition;
+            if (mousePos.x != oldMouseX)
+            {
+                yawAng += (mousePos.x - oldMouseX) * xDeltaFactor;
+            }
+            if (mousePos.y != oldMouseY)
+            {
+                pitchAng += (mousePos.y - oldMouseY) * yDeltaFactor;
+            }
+            oldMouseX = mousePos.x;
+            oldMouseY = mousePos.y;
         }
 
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, turnPos, 0), turnRate);
+        if (Mathf.Abs(pitchAng) >= 60)
+        {
+            pitchAng = (pitchAng / Mathf.Abs(pitchAng)) * 60;
+        }
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(pitchAng, yawAng, 0), turnRate);
 
     }
 }
